9e0c194785a506e58726a604ac64980bfee6ec73
ranmat <ranmat11@gmail.com>
2014-07-30 12:31:00 +0100

    Update script.js

--- app/script.js
+++ app/script.js
@@ -7,6 +7,17 @@ xmlhttp.onreadystatechange=function () {
     var num = json[1].length;
   
     console.log("Number of hic: " + num);
+    
+    for (var i in json[1]) {
+      var item = json[1][i];
+      var latlong = new google.maps.LatLng(item.latitude,item.longitude);
+      
+      var marker = new google.maps.Marker({
+          position: latlong,
+          map: map,
+          title:item.name
+      });
+    }
   }
 }
 xmlhttp.send(null);

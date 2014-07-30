7bdb52538d7d04499fa06648e9c00a2bfa02db56
ranmat <ranmat11@gmail.com>
2014-07-30 12:38:49 +0100

    Update script.js

--- app/script.js
+++ app/script.js
@@ -13,7 +13,7 @@ xmlhttp.onreadystatechange=function () {
       var latlong = new google.maps.LatLng(item.latitude,item.longitude);
       
       var populationOptions = {
-        strokeColor: '#FF0000',
+        strokeColor: '#00FF00',
         strokeOpacity: 0.8,
         strokeWeight: 2,
         fillColor: '#FF0000',

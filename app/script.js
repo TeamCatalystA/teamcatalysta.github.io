19d3064eb5af511fb98897698182925e95623152
ranmat <ranmat11@gmail.com>
2014-07-30 12:06:58 +0100

    Update script.js

--- app/script.js
+++ app/script.js
@@ -5,7 +5,7 @@ var xmlhttp = new XMLHttpRequest();
 xmlhttp.open("GET", "hic.json",true);
 xmlhttp.onreadystatechange=function() {
   if (xmlhttp.readyState==4) {
-    var json = JSO.parse(xmlhttp.responseText);
+    var json = JSON.parse(xmlhttp.responseText);
     
     console.log(json);
   }

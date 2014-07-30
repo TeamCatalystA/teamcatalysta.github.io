83b9d5f9f202e2a1074cc60b5174d73778a4a350
ranmat <ranmat11@gmail.com>
2014-07-30 12:43:47 +0100

    Update script.js

--- app/script.js
+++ app/script.js
@@ -1,30 +1,36 @@
-var xmlhttp = new XMLHttpRequest();
-xmlhttp.open("GET", "hic.json",true);
-xmlhttp.onreadystatechange=function () {
-  if (xmlhttp.readyState==4) {
-    var json = JSON.parse(xmlhttp.responseText);
-      
-    var num = json[1].length;
-  
-    console.log("Number of hic: " + num);
+function go(url, colour, colourAlt) {  
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.open("GET", url,true);
+  xmlhttp.onreadystatechange=function () {
+    if (xmlhttp.readyState==4) {
+      var json = JSON.parse(xmlhttp.responseText);
+        
+      var num = json[1].length;
     
-    for (var i in json[1]) {
-      var item = json[1][i];
-      var latlong = new google.maps.LatLng(item.latitude,item.longitude);
+      console.log("Number of hic: " + num);
       
-      var populationOptions = {
-        strokeColor: '#007F00',
-        strokeOpacity: 0.8,
-        strokeWeight: 2,
-        fillColor: '#00FF00',
-        fillOpacity: 0.35,
-        map: map,
-        center: latlong,
-        radius: 100000
-      };
-      // Add the circle for this city to the map.
-      var cityCircle = new google.maps.Circle(populationOptions);
+      for (var i in json[1]) {
+        var item = json[1][i];
+        var latlong = new google.maps.LatLng(item.latitude,item.longitude);
+        
+        var populationOptions = {
+          strokeColor: colourAlt,
+          strokeOpacity: 0.8,
+          strokeWeight: 2,
+          fillColor: colour,
+          fillOpacity: 0.35,
+          map: map,
+          center: latlong,
+          radius: 100000
+        };
+        // Add the circle for this city to the map.
+        var cityCircle = new google.maps.Circle(populationOptions);
+      }
     }
   }
+  xmlhttp.send(null);
 }
-xmlhttp.send(null);
+
+go("hic.json", "#00ff00", "#007f00");
+go("mic.json", "#228888", "#116666");
+go("lic.json", "#ff0000", "#7f0000")

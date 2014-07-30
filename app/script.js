047edbee1a0c2ad22db2a3edebfa9df38fc233be
Zenphos <bigboo1099@yahoo.co.uk>
2014-07-30 22:08:53 +0100

    Update script.js

--- app/script.js
+++ app/script.js
@@ -30,15 +30,20 @@ function go(url, colour, colourAlt) {
         };
         var xmlhttp2 = new XMLHttpRequest();
         xmlhttp2.open("GET", "Homicides.json" ,true);
+        var x = 2
+        do {
         xmlhttp2.onreadystatechange=function () {
         if (xmlhttp.readyState==4) {
           var t = JSON.parse(xmlhttp.responseText);
         
           var num = t[1].length;
           console.log("5");
+          x = 1
           xmlhttp2.send(null);
         }
         }
+        }
+        while x <> 1
         var cityCircle = new google.maps.Circle(populationOptions);
       }
     }

c6c1cbe57e5685e45e0be436dfab7d8beb7a5da6
Zenphos <bigboo1099@yahoo.co.uk>
2014-07-30 12:11:56 +0100

    Update Map.js

--- app/Map.js
+++ app/Map.js
@@ -1,3 +1,6 @@
+
+window.onload = function () {
+
 function initialize() {
   
   var mapOptions = {
@@ -5,10 +8,11 @@ function initialize() {
                 center: new google.maps.LatLng(0, 0)
             };
 
-            var map = new google.maps.Map("index.html".getElementById('map-canvas'),
+            var map = new google.maps.Map(document.getElementById('map-canvas'),
     mapOptions);
 
   
 }
 
         google.maps.event.addDomListener(window, 'load', initialize);
+}

b00e2a1062ef8ffd170c5fc824286e3b7dc7b76c
ranmat <ranmat11@gmail.com>
2014-07-30 10:56:03 +0100

    Update script.js

--- app/script.js
+++ app/script.js
@@ -5,9 +5,10 @@ var xmlhttp = new XMLHttpRequest();
 xmlhttp.open("GET", "Data.xml",true);
 xmlhttp.onreadystatechange=function() {
   if (xmlhttp.readyState==4) {
-    var $xml = $($.parseXML(xmlhttp.responseText));
+    parser=new DOMParser();
+    xmlDoc=parser.parseFromString(xmlhttp.readyState,"text/xml");
     
-    console.log($xml.find("ListRecords"));
+    console.log(xmlDoc);
   }
 }
 xmlhttp.send(null);

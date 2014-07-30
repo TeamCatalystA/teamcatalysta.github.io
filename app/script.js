084bbb614cf13552e3cd4aa61b39bd652f8b3bfb
ranmat <ranmat11@gmail.com>
2014-07-30 10:50:20 +0100

    Update script.js

--- app/script.js
+++ app/script.js
@@ -1,10 +1,11 @@
 console.log("Init");
 
-$.ajax({
-  url: "Data.xml"
-})
-  .done(function( data ) {
-    if ( console && console.log ) {
-      console.log( "Sample of data:", data.slice( 0, 100 ) );
-    }
-  });
+var xmlhttp = new XMLHttpRequest();
+
+xmlhttp.open("GET", "test.txt",true);
+xmlhttp.onreadystatechange=function() {
+  if (xmlhttp.readyState==4) {
+   alert(xmlhttp.responseText);
+  }
+}
+xmlhttp.send(null);

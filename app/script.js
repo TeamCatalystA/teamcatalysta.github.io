4c28062afc8964079c5659ad9522596ca608cbff
Zenphos <bigboo1099@yahoo.co.uk>
2014-07-30 11:12:14 +0100

    Update script.js

--- app/script.js
+++ app/script.js
@@ -2,7 +2,7 @@ console.log("Init");
 
 var xmlhttp = new XMLHttpRequest();
 
-xmlhttp.open("GET", "http://r4d.dfid.gov.uk/Gateway/?verb=ListRecords&metadataPrefix=oai_dc",true);
+xmlhttp.open("GET", "Data.xml",true);
 xmlhttp.onreadystatechange=function() {
   if (xmlhttp.readyState==4) {
     parser=new DOMParser();

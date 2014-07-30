cbb95664041f83f15810f7aeec5cdf1af8c83f67
ranmat <ranmat11@gmail.com>
2014-07-30 12:09:00 +0100

    Update script.js

--- app/script.js
+++ app/script.js
@@ -7,7 +7,10 @@ xmlhttp.onreadystatechange=function() {
   if (xmlhttp.readyState==4) {
     var json = JSON.parse(xmlhttp.responseText);
     
-    console.log(json);
+    var arr = json[1];
+    for (var item in array) {
+      console.log(item.name + ": " + item.incomeLevel.value)
+    }
   }
 }
 xmlhttp.send(null);

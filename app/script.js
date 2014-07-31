56a3dac58f7b5b46859d74c8ba6f0fc8a6adc71e
Zenphos <bigboo1099@yahoo.co.uk>
2014-07-31 13:33:10 +0100

    Update script.js

--- app/script.js
+++ app/script.js
@@ -85,7 +85,7 @@ function Tag(contentString) {
 
 function refresh(){
   
-clearOverlays()
+cityCircle.setmap(null);
 go("hic.json", "#00ff00", "#007f00");
 go("mic.json", "#ffc200", "#7f1000");
 go("lic.json", "#ff0000", "#7f0000");

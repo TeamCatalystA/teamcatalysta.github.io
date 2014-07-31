4c70289d933a8dc0cd5406813619e292a1e1b06c
Zenphos <bigboo1099@yahoo.co.uk>
2014-07-31 12:11:13 +0100

    Update script.js

--- app/script.js
+++ app/script.js
@@ -49,7 +49,7 @@ function go(url, colour, colourAlt) {
         };
         var info = ("Number of Homicides = " + x)
         var marker = new google.maps.Marker({
-      position: latlong,
+      position: myLatlong,
       map: map,
       title: 'Uluru (Ayers Rock)'
   });

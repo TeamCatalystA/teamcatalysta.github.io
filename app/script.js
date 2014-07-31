cdf46beaefdeb5e4471ce997d102249a0c30a1b9
Zenphos <bigboo1099@yahoo.co.uk>
2014-07-31 11:18:34 +0100

    Update script.js

--- app/script.js
+++ app/script.js
@@ -5,7 +5,8 @@ function Data(url, country){
     if (r.readyState==4) {
       var data = JSON.parse(r.responseText);
       var num = data.length;
-      for (var i in data[1]) {
+      console.log(num)
+      for (var i in num) {
   
   
         var item = data[i]
